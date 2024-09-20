ceceab7802db30c669c5e215c63796d36ec479c6
Paul Hoger <paul.hoger@student.kit.edu>
2024-09-20 20:15:06 +0200

    Implement start screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,55 @@
+import {useState} from "react";
+
 export default function App() {
+    const [username, setUsername] = useState("")
+    const [clicked, setClicked] = useState(false)
+
+    function searchGame() {
+        setClicked(true)
+
+        console.log(username)
+    }
+
     return (
-        <h1 className="text-3xl font-header underline">
-            Hello world!
-        </h1>
+        <div className="p-8">
+            <div className="font-title uppercase text-4xl flex flex-col text-white">
+                <div
+                    className="self-start drop-shadow-[5px_5px_5px_rgba(0,0,0,1)] animate-move-left-to-right">Approaching
+                </div>
+                <div className="self-end drop-shadow-[5px_5px_5px_rgba(0,0,0,1)] animate-move-right-to-left">Words</div>
+            </div>
+
+            <div className="font-header flex flex-col items-center pt-8">
+                <div className="text-white text-3xl">How does it work?</div>
+                <div className="pt-5">
+                    Together with a random player, you have to come up with the same word.
+                    To do this, you have to get closer word by word.
+                </div>
+            </div>
+
+            <div className="font-header flex flex-col items-center pt-8">
+                <div className="text-white text-3xl">Let's getting started!</div>
+                <div className="pt-5">
+                    Choose your username and search for a game.
+                </div>
+                <div className="flex flex-col w-full pl-16 pr-16 pt-5">
+                    <input className="rounded p-2 border border-black" placeholder="e.g., SharebinAPI"
+                           onChange={(e) => setUsername(e.target.value)}
+                           disabled={clicked}
+                    />
+                    {!clicked ?
+                        <button
+                            className="bg-btn border-btn-border text-white rounded-2xl border-4 w-full mt-5 p-1"
+                            onClick={() => searchGame()}
+                        >Search
+                        </button> :
+                        <button
+                            className="bg-btn-active border-btn-border-active text-white rounded-2xl border-4 w-full mt-5 p-1"
+                        >Searching...
+                        </button>
+                    }
+                </div>
+            </div>
+        </div>
     )
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,8 +7,31 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        "header": ["Luckiest Guy"],
-      }
+        "title": ["Luckiest Guy"],
+        "header": ["Roboto"],
+      },
+      colors: {
+        "background-from": "#D05757",
+        "background-to": "#FFE3E3",
+        "btn": "#FF5C5C",
+        "btn-border": "#BA0505",
+        "btn-active": "#CA7C7C",
+        "btn-border-active": "#340303"
+      },
+      keyframes: {
+        moveLeftToRight: {
+          '0%, 100%': { transform: 'translateX(0)' },
+          '50%': { transform: 'translateX(30px)' },
+        },
+        moveRightToLeft: {
+          '0%, 100%': { transform: 'translateX(0)' },
+          '50%': { transform: 'translateX(-30px)' },
+        },
+      },
+      animation: {
+        'move-left-to-right': 'moveLeftToRight 5s ease-out infinite',
+        'move-right-to-left': 'moveRightToLeft 5s ease-out infinite',
+      },
     },
   },
   plugins: [],
