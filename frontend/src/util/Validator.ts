5c238a0fe6963106e0c6ce45a0ecfb2fcb634e6c
Paul Hoger <paul.hoger@gmx.net>
2024-11-09 17:12:59 +0100

    Allow ö, ä, ü, and ß

--- frontend/src/util/Validator.ts
+++ frontend/src/util/Validator.ts
@@ -6,7 +6,7 @@ export function validateUsername(username: string): string | undefined {
         return "The username is too long. Only 16 characters are allowed."
     }
 
-    const regex = /^[a-zA-Z0-9_-]+$/
+    const regex = /^[a-zA-Z0-9üäöß_-]+$/i
     if (!regex.test(username)) {
         return "The username contains illegal characters. Only alphanumeric characters, -, and _ are allowed."
     }
@@ -22,7 +22,7 @@ export function validateGuess(word: string): string | undefined {
         return "The word is too long. Only 32 characters are allowed."
     }
 
-    const regex = /^[a-zA-Z-]+$/
+    const regex = /^[a-zA-Z0-9üäöß_-]+$/i
     if (!regex.test(word)) {
         return "The word contains illegal characters. Only letters and - are allowed."
     }
