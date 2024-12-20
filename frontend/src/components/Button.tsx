175186aa536485761e633fd31d7af6e4c8ce4ebd
Paul Hoger <paul.hoger@student.kit.edu>
2024-12-20 23:05:11 +0100

    Change cursor on clicked buttons back to default

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -10,7 +10,7 @@ export default function Button(props: ButtonProps) {
     if (props.clicked) {
         return (
             <button
-                className="bg-btn-active border-btn-border-active text-white rounded-2xl border-4 w-full mt-5 p-1"
+                className="bg-btn-active border-btn-border-active text-white rounded-2xl border-4 w-full mt-5 p-1 cursor-default"
             >{props.clickedText}
             </button>
         )
